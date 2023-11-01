c3bdf178235304031605e4f22d558170148b4451
diegocamargo55555 <diegocamargo55555@gmail.com>
2023-11-01 07:43:56 -0300

    atividade 6 ex 6 completo

--- atividade_6/1.cc
+++ atividade_6/1.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "funcoes.h"
+#include <string>
+
+
 using namespace std;
 
 int main()
 {
-    /* EX 1
+/* EX 1
     int a = 5, b = 8;
     ordem(&b,&a);
 
@@ -17,7 +20,34 @@ int main()
     int_frac(&a);
 */
 
-    int aray[5] = {5,8,0,11,4};
+/* EX 2 B
+    int aray[] = {5,8,0,13,4};
     cout << has_zero(aray, 5) << endl;
+*/
+/* EX 2 C1
+    int aray[5] = {5,8,0,13,4};
+    maior(aray, 5);
+*/
+/* EX 2 C2
+    int aray[5] = {5,8,0,13,4};
+    media(aray, 5);
+*/
+/* EX 2 C3
+    int aray[5] = {5,8,0,13,4};
+    positivos(aray,5);
+*/
+/* Ex 2 C4
+    int aray[5];
+    input_array(aray, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << aray[i] << " ";
+    }
+    cout << endl;
+*/
+/* EX 2 C5
+    int aray[5] = {5,8,0,13,4};
+    maior_menor(aray, 5);
+*/
 
 }
--- atividade_6/funcoes.h
+++ atividade_6/funcoes.h
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//ex1
 void ordem(int *a, int *b)
 {
     if (a < b)
@@ -13,7 +13,7 @@ void ordem(int *a, int *b)
         b = a;
     }
 }
-
+// EX 2 A
 void int_frac(double *a)
 {
     int inteiro = *a;
@@ -22,19 +22,86 @@ void int_frac(double *a)
     cout << *a << endl;
 
 }
-
-
-
-bool has_zero(int *a, int n) 
-{
-    int i;
-    for (i = 0; i < n; i++)
+// ex 2 B
+bool has_zero(int a[], int n) {
+    int i, verif;
+    for (i = 0; i < n; i++){
         if (a[i] == 0){
-            return true;
-            cout << "true" << endl;
+            verif = 1;
         }
-        else{
-            return false;
-            cout << "true" << endl;
+    }
+    if (verif == 1)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+    
+}   
+// EX 2 C1
+
+void maior(int a[], int n)
+{
+    int maior = INT16_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > maior)
+        {
+            maior = a[i];
         }
+    }
+    cout << maior << endl;
+}
+//  EX C2
+void media(int a[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+    cout << sum/n << endl;
+}
+//  EX C3
+void positivos(int a[], int n)
+{
+    int positivos = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+        {
+            positivos += 1;
+        }
+    }
+    cout << positivos << endl;
+}
+
+void input_array(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+}
+
+void maior_menor(int *a, int n)
+{
+    int maior = INT16_MIN, menor = INT16_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > maior)
+        {
+            maior = a[i];
+        }
+        if (a[i] < menor)
+        {
+            menor = a[i];
+        }
+        
+    }
+    cout << maior << endl << menor << endl;
+    
 }
