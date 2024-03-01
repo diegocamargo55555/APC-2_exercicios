d1fe45e47bb30e9a89b7266a47be74612faff6ac
diegocamargo55555 <137927984+diegocamargo55555@users.noreply.github.com>
2024-02-29 23:07:10 -0300

    Create 4.cc

---
+++ atividade_16/4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+const double PI = 3.14159;
+
+int main() {
+    double radius = 5.0; // Raio do círculo
+
+    // Calcula a área do círculo usando a constante PI
+    double area = PI * radius * radius;
+
+    std::cout << "A área do círculo é: " << area << std::endl;
+
+    return 0;
+}
